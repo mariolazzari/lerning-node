7463f63d1acdc6fc67a547ab36fced615f295180
mariolazzari <mario.lazzari@gmail.com>
2018-08-19 12:20:30 +0200

    Done chapter 2

---
+++ module-demo.js
@@ -0,0 +1,4 @@
+const myModule = require("./my-module");
+
+console.log(myModule);
+console.log(myModule.myText);
---
+++ my-module.js
@@ -0,0 +1,6 @@
+exports.myText = "Hello from my-module";
+
+/*
+const myText = "Hello from my-module";
+module.exports = { myText };
+*/

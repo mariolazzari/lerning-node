3a56a0d6949cf235d282ff4cd8e2f476004c55f3
mariolazzari <mario.lazzari@gmail.com>
2018-08-19 15:50:14 +0200

    Done chapter 8

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+// db connection
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(
+    "mongodb://localhost:27017/learning-node",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB connected."))
+  .catch(err => console.log("MongoDB connection error:", err));
 
 // express settings
 const app = express();
@@ -7,33 +18,70 @@ const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
 app.use(express.static(__dirname));
-app.use(bodyParser.urlencoded({ urlencoded: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// message model
+const Message = mongoose.model("Message", {
+  name: String,
+  message: String
+});
+
 // server routes
-const messages = [
-  {
-    name: "Mario",
-    message: "Hi"
-  },
-  {
-    name: "Mary",
-    message: "Hello"
-  }
-];
-
 app.get("/messages", (req, res) => {
-  res.send(messages);
+  Message.find({})
+    .then(messages => res.send(messages))
+    .catch(err => console.log(err));
 });
 
+// async version
+app.post("/messages", async (req, res) => {
+  try {
+    const message = new Message(req.body);
+    // save
+    console.log("Message saved");
+    const savedMessage = await message.save();
+    // censored
+    console.log("Check for bad words");
+    const censored = await Message.findOne({ message: "badword" });
+
+    if (censored) {
+      console.log("Censored word found", censored);
+      await Message.findOneAndDelete({ _id: censored._id });
+    } else {
+      io.emit("message", req.body);
+    }
+    res.sendStatus(200);
+  } catch (error) {
+    sendStatus(500);
+    return console.err(error);
+  } finally {
+    console.log("Post called succesfully");
+  }
+});
+
+/*
 app.post("/messages", (req, res) => {
-  messages.push(req.body);
-
-  console.log(req.body);
-
-  io.emit("message", req.body);
-  res.sendStatus(200);
+  const msg = new Message(req.body);
+  msg
+    .save()
+    .then(() => {
+      console.log("Check for bad words");
+      return Message.findOne({ message: "badword" });
+    })
+    .then(censored => {
+      if (censored) {
+        console.log("Censored word found", censored);
+        return Message.findOneAndDelete({ _id: censored._id });
+      }
+      console.log("Message saved");
+      io.emit("message", req.body);
+      res.sendStatus(200);
+    })
+    .then(deleted => console.log("Deleted censored word", deleted))
+    .catch(err => res.sendStatus(500));
 });
+*/
 
 // start server
 const server = http.listen(3000, () =>
